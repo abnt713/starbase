6b797399583af4db27912988b1529f20a92b918b
Alison Bento <alisonbnt@gmail.com>
2022-02-21 22:38:39 -0300

    improve dummy nvim and add provider sample file

---
+++ starbase/Provider.sample.lua
@@ -0,0 +1,17 @@
+-- This one is a custom provider. Use this instance to override any setting.
+-- It is important to place this file in the import path "starbase.Provider"
+Provider = require('starbase.app.Provider'):new()
+
+-- This method, for example, overrides the default theme setting and uses the Rigel theme.
+-- Uncomment the lines below to override the theme.
+-- function Provider.theme(self)
+--   return self:provide('theme', function()
+--     return require('starbase.theme.Rigel').new(
+--       self:nvim(),
+--       self:plugin_manager(),
+--       self:statusline()
+--     )
+--   end)
+-- end
+
+return Provider
--- starbase/dummy/nvim.lua
+++ starbase/dummy/nvim.lua
@@ -2,6 +2,7 @@ return {
   api = {
     nvim_set_keymap = function() return '' end,
   },
+  cmd = function() return nil end,
   fn = {
     exists = function() return true end,
     getcwd = function() return '' end,
