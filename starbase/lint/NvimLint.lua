04ab756262070d99fbcc6e8b71244865536d52a6
Alison Bento <alisonbnt@gmail.com>
2022-02-22 18:28:50 -0300

    add linter layer

---
+++ starbase/lint/NvimLint.lua
@@ -0,0 +1,30 @@
+local NvimLint = {}
+NvimLint.__index = NvimLint
+
+function NvimLint.new(nvim, plugin_manager)
+  return setmetatable({
+    nvim = nvim,
+    plugin_manager = plugin_manager,
+  }, NvimLint)
+end
+
+function NvimLint.configure(self)
+  self.plugin_manager:add_dependency('mfussenegger/nvim-lint', self:setup_plugin())
+end
+
+function NvimLint.setup_plugin(self)
+  return function()
+    local lint = self.plugin_manager:require('lint')
+    if not lint then return end
+
+    lint.linters_by_ft = {
+      'golangcilint',
+      'luacheck',
+    }
+    self.nvim.api.nvim_exec([[
+      au BufWritePost <buffer> lua require('lint').try_lint()
+    ]], false)
+  end
+end
+
+return NvimLint
